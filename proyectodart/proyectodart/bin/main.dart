11908ee10cfcda271e98067bbe51734e8986b64d
zabdielcead <zabdiel.cead@gmail.com>
2019-12-13 14:56:57 -0600

    sube http dep

--- proyectodart/proyectodart/bin/main.dart
+++ proyectodart/proyectodart/bin/main.dart
@@ -1,13 +1,35 @@
-import 'package:proyectodart/proyectodart.dart' as proyectodart; // proyectodart es alias 
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+
+
+import 'package:proyectodart/proyectodart.dart' as proyectodart; // proyectodart es alias 
 main(List<String> arguments) {
-  print('Hello world: ${proyectodart.calculate()}!');
+  //print('Hello world: ${proyectodart.calculate()}!');
 
   /*
     estructura de unn proyecto dart 
 
     bin/main.dart   es el punto inicial de la aplicacion 
     test/ **_test.dart  archivos de prueba 
-  
+
+
+
+    librerias para http
+
+    https://pub.dev/    -> repositorio de paquetes de dart
+
+    pub get -> baja dependiencias 
+
+
+    rest api https://reqres.in/
+
    */
+    final url = 'https://reqres.in/api/users?page=2';
+    http.get(url).then( (res) {
+      print(res);
+    });
+
+
+
 }
